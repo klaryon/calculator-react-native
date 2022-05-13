c82f3865ae1b03deef7fd44d0cac26e5cccd8ed7
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-05-13 11:43:22 +0200

    result texts and initial button

--- src/screens/CalculatorScreen.tsx
+++ src/screens/CalculatorScreen.tsx
@@ -4,8 +4,15 @@ import {styles} from '../theme/appTheme';
 
 export const CalculatorScreen = () => {
   return (
-    <View>
-      <Text style={styles.text}>Screen calculator</Text>
+    <View style={styles.calculatorContainer}>
+      <Text style={styles.resultSmall}>1,500.00</Text>
+      <Text style={styles.result}>1,500.00</Text>
+
+      <View>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>1</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -5,8 +5,33 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  text: {
+  calculatorContainer: {
+    flex: 1,
+    paddingHorizontal: 10,
+    justifyContent: 'flex-end',
+  },
+  result: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  resultSmall: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  button: {
+    height: 80,
+    width: 80,
+    backgroundColor: '#9B9B9B',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  buttonText: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: '300',
   },
 });

ee95f55dde2706fad973b1238a79f2223fe7e9da
Clarissa Ardon <clarissa.ardon@crealogix.com>
2022-05-13 10:48:46 +0200

    calcultor initial theme setup

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {SafeAreaView, StatusBar} from 'react-native';
+import {CalculatorScreen} from './src/screens/CalculatorScreen';
+import {styles} from './src/theme/appTheme';
 
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.background}>
+      <StatusBar backgroundColor="black" barStyle="light-content" />
+      <CalculatorScreen />
+    </SafeAreaView>
   );
 };
 
---
+++ src/screens/CalculatorScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+import {styles} from '../theme/appTheme';
+
+export const CalculatorScreen = () => {
+  return (
+    <View>
+      <Text style={styles.text}>Screen calculator</Text>
+    </View>
+  );
+};
